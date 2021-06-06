58ea02c19df1c4d6ec95c8f3cab72bb0db31b271
haikü <olliedawes@gmail.com>
2021-06-06 19:38:33 +0100

    Add constants file

---
+++ web/release/Metonym/Constants.js
@@ -0,0 +1,4 @@
+.pragma library
+
+const CANONIC_DARK_THEME_SOURCE = 'https://www.canonic.com/metonym/release/Metonym/CanonicDarkTheme.qml'
+const CANONIC_LIGHT_THEME_SOURCE = 'https://www.canonic.com/metonym/release/Metonym/CanonicLightTheme.qml'
